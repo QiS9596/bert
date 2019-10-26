2fdb4cab8a75e19ba2fce4848ab00624c800fe68
Qi Song <rolo_lamperouge@foxmail.com>
2019-10-26 15:29:29 -0400

    sync

--- extract_vp_embedding.py
+++ extract_vp_embedding.py
@@ -2,6 +2,7 @@ import pandas as pd
 import tensorflow as tf
 import os
 import argparse
+import pandas as pd
 
 parser = argparse.ArgumentParser(description='BERT feature extraction for vp dataset')
 parser.add_argument('-method', type=str, default='concat4',
@@ -11,9 +12,9 @@ parser.add_argument('-method', type=str, default='concat4',
 parser.add_argument('-bert-model', type=str, default='./uncased_L-12_H-768_A-12',
                     help="""Directory for bert model config file and vocab list
                     [default:./uncased_L-12_H-768_A-12""")
-parser.add_argument('-input', type=str, default='./data/vp/all.tsv',
-                    help="""Directory for input file, should be tsv file for vp data
-                    [default:./data/vp/all.tsv]""")
+parser.add_argument('-input', type=str, default='./data/vp/',
+                    help="""Directory for input file, script will search for all.tsv and labels.txt in this dir
+                    [default:./data/vp/]""")
 parser.add_argument('-temp-dir', type=str, default='./tmp/vp_extract',
                     help="""temporary directory for holding intermidiate files
                     [default:./tmp/vp_extract]""")
@@ -22,3 +23,21 @@ parser.add_argument('-output-dir', type=str, default='./data/vp/bert_embeddings'
                     [default:./data/vp/vert_embeddings]""")
 parser.add_argument('-sequence-length', type=int, default=64,
                     help='padded sequence length [default:64]')
+
+
+args = parser.parse_args()
+training_data_path = os.path.join(args.input,'all.tsv')
+labels_data_path = os.path.join(args.input, 'labels.txt')
+
+TRAIN_TEXT_NAME = os.path.join(args.temp_dir, 'train_text.txt')
+
+df_train = None
+df_labels = None
+try:
+    df_train = pd.read_csv(training_data_path, sep='\t', header=None, names=['labels','text'])
+    df_labels = pd.read_csv(labels_data_path, sep='\t', header=None, names=['labels','text'])
+except None:
+    exit()
+
+text_train = df_train['text']
+text_train.to_csv(TRAIN_TEXT_NAME, )
